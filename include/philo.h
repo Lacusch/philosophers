7806d86244a83316019ee9c35832a52607f75f5a
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-11-25 14:01:28 +0100

    basic imput parcing + saving input in a struct

--- include/philo.h
+++ include/philo.h
@@ -1,1 +1,17 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// typedef enum bool
+// {
+// 	int 
+// }
+
+typedef struct philo
+{
+	int t_to_die;
+	int t_to_eat;
+	int t_to_sleep;
+	bool option;
+	int times_to_eat;
+}	t_philo;
--- src/main.c
+++ src/main.c
@@ -1,22 +1,44 @@
 #include "../include/philo.h"
+void philo_printf(t_philo *philo);
 
+int parce_input(int ac, char **av, t_philo *philo)
+{
+		// printf("ac is:%i\n", ac);
+		if (ac < 5)
+		{
+			printf ("not enough arguments\n");
+			return (1);
+		}
+		else if (ac > 6)
+		{
+			printf ("too many arguments\n");
+			return (1);
+		}
+		philo->t_to_die = atoi(av[2]);
+		philo->t_to_eat = atoi(av[3]);
+		philo->t_to_sleep = atoi(av[4]);
+		if (ac == 6)
+		{
+			philo->option = true;
+			philo->times_to_eat = atoi(av[5]);
+		}
+	return (0);
+}
 int main (int ac, char **av)
 {
-	if (av != NULL)
-	printf("ac is%i\n", ac);
-	if (ac < 5)
-	{
-		printf ("not enough arguments\n");
+	t_philo philo;
+	if (parce_input(ac, av, &philo))
 		return (1);
-	}
-	if (ac == 5)
-		printf ("1. case\n");
-	if (ac == 6)
-		printf ("2. case\n");
-	if (ac > 6)
-	{
-		printf ("too many arguments\n");
-		return (1);
-	}
+	philo_printf(&philo);
 	return (0);
+}
+
+void philo_printf(t_philo *philo)
+{
+	printf("%d\n", philo->t_to_die);
+	printf("%d\n", philo->t_to_eat);
+	printf("%d\n", philo->t_to_sleep);
+	printf("%d\n", philo->t_to_eat);
+	if (philo->option)
+		printf("%d", philo->times_to_eat);
 }
