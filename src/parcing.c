19854902a3c0d3d0fa27e15e9dc02f31bac0a40b
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-12-12 10:44:41 +0100

    More robust error checking.

--- src/parcing.c
+++ src/parcing.c
@@ -6,22 +6,48 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 16:55:06 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/12/05 14:36:39 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/12/12 10:44:25 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+bool printf_error(char* str)
+{
+	if (str == NULL)
+		printf("Error\n");
+	else
+		printf("Error\n%s\n", str);
+	return (true);
+}
+
+bool wrong_input(t_data *data)
+{
+	if (data->philo_nb == 0)
+		return (printf_error("no philo"));
+	else if (data->philo_nb < 0)
+		return (printf_error("negative philo nb"));
+	else if (data->t_to_die < 0)
+		return (printf_error("negative time to die"));
+	else if (data->t_to_eat < 0)
+		return (printf_error("negative time to eat"));
+	else if (data->t_to_sleep < 0)
+		return (printf_error("negative time to sleep"));
+	else if (data->option && data->times_to_eat < 0)
+		return (printf_error("the amout of times to eat is negative"));
+	else
+		return (false);
+}
 int	parce_input(int ac, char **av, t_data *data)
 {
 	if (ac < 5)
 	{
-		printf ("not enough arguments\n");
+		printf_error("not enough arguments");
 		return (EXIT_FAILURE);
 	}
 	else if (ac > 6)
 	{
-		printf ("too many arguments\n");
+		printf_error ("too many arguments");
 		return (EXIT_FAILURE);
 	}
 	data->philo_nb = atoi(av[1]);
@@ -33,5 +59,7 @@ int	parce_input(int ac, char **av, t_data *data)
 		data->option = true;
 		data->times_to_eat = atoi(av[5]);
 	}
+	if (wrong_input(data))
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
