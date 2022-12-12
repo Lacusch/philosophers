f60efb9a9d9c1429e9f81bfa28458876ad285ed1
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-12-12 11:02:56 +0100

    added readme

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 15:15:55 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/12/12 10:45:30 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/12/12 10:54:37 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ int	main(int ac, char **av)
 	t_data	data;
 	t_philo			*philos;
 
-	philos = NULL;
-
 	if (parce_input(ac, av, &data))
 		return (EXIT_FAILURE);
 	init_data(&data);
+	philos = malloc(sizeof(t_philo) * data.philo_nb);
+	if (philos == NULL)
+		return(free(philos), printf_error("philo allocation failed"));
 	philos = create_philos(&data);
+	//Also free philo data
+	if (philos == NULL)
+		return(free(philos), printf_error("philo allocation failed"));
 	create_threads(philos);
 	free(philos);
 	return (EXIT_SUCCESS);
